bd8783cf8470af2ffd8bbc4063117104c7996c6f
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-11-09 20:35:31 +0700

    setup vue material

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './vuex/store'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
+
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
 
